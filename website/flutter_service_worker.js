0fb26ec5840624e76f9fb8d6861625dfa1520352
shohjahon <komiljonovshohjahon@gmail.com>
2023-05-17 15:11:20 +0900

    update website 2023-05-17 15:09:38

--- website/flutter_service_worker.js
+++ website/flutter_service_worker.js
@@ -525,9 +525,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "12f52ebab27ac05ec7665ad654af777b",
-"/": "12f52ebab27ac05ec7665ad654af777b",
-"main.dart.js": "5488d2ec57032966cb35847e9a6bd50b",
+"index.html": "651762b9b66e9ab0e45b53a7c867d008",
+"/": "651762b9b66e9ab0e45b53a7c867d008",
+"main.dart.js": "5f948ee9d4973ddb6910ebc98746b7e4",
 "manifest.json": "ae6b44f605b9c04a6e90180a8ad956b0",
 "styles.css": "37387c589b05828439fb1417dc7a02bb",
 "version.json": "7fab6da096fd3c24cb62b70511fa7519"
