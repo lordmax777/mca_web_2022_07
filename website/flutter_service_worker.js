79d283c5eb3ecde04cc03449f40a3b21adc755f8
shoh <komiljonovshohjahon@gmail.com>
2023-07-13 11:14:32 +0900

    update website 2023-07-13 11:13:20

--- website/flutter_service_worker.js
+++ website/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c7bdb77c988d926145651d6bb85d761a
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6780e47d1ac9dbf8cce2da2933465e26",
-"/": "6780e47d1ac9dbf8cce2da2933465e26",
-"main.dart.js": "b07b964e904b32842722287780754791",
+"index.html": "4f8d15db4be4b31623c538366095c889",
+"/": "4f8d15db4be4b31623c538366095c889",
+"main.dart.js": "3f93bab8c48fa0a0b23ae4e326fac90f",
 "manifest.json": "bf6f338428a8072c9d98c15107136a44",
 "styles.css": "37387c589b05828439fb1417dc7a02bb",
 "version.json": "31a3aa7f19c181c1f19c1330c44a13f0"};
