b6d81fb6f52cfabcfb34217225567b439dd0b2b5
Shokhjakhon <komiljonovshohjahon@gmail.com>
2023-10-25 17:23:24 +0500

    update

--- lib/presentation/pages/clients_view/dialogs/clients_edit_dialog.dart
+++ lib/presentation/pages/clients_view/dialogs/clients_edit_dialog.dart
@@ -1,7 +1,7 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:mca_dashboard/manager/data/data.dart';
 import 'package:mca_dashboard/manager/manager.dart';
-import 'package:mca_dashboard/presentation/global_widgets/default_dialog.dart';
+import 'package:mca_dashboard/presentation/global_widgets/widgets.dart';
 import 'package:mca_dashboard/presentation/pages/scheduling_view/schedule_widgets/shift_card.dart';
 
 class ClientsEditDialog extends StatefulWidget {
@@ -19,40 +19,42 @@ class _ClientsEditDialogState extends State<ClientsEditDialog> {
   //name
   String name = "";
   //company
-  final TextEditingController companyController = TextEditingController();
+  String company = "";
   //email
-  final TextEditingController emailController = TextEditingController();
+  String email = "";
   //phone
-  final TextEditingController phoneController = TextEditingController();
+  String phone = "";
+  //fax
+  String fax = "";
 
   //address1
-  final TextEditingController address1Controller = TextEditingController();
+  String address1 = "";
   //address2
-  final TextEditingController address2Controller = TextEditingController();
+  String address2 = "";
   //city
-  final TextEditingController cityController = TextEditingController();
+  String city = "";
   //county
-  final TextEditingController countyController = TextEditingController();
+  String county = "";
   CountryMd? country;
   //postcode
-  final TextEditingController postcodeController = TextEditingController();
+  String postcode = "";
 
   //Currency
   CurrencyMd? currency;
   //payment method
   PaymentMethodMd? paymentMethod;
   //payment due
-  final TextEditingController paymentDueController = TextEditingController();
+  String paymentDue = "";
   //Invoice Period
   InvoicePeriod? invoicePeriod;
   //Invoice Day
-  final TextEditingController invoiceDayController = TextEditingController();
+  String invoiceDay = "";
   //combine invoice
   bool isCombineInvoices = false;
   //send invoices
   bool isSendInvoices = false;
   //comment
-  final TextEditingController commentController = TextEditingController();
+  String comment = "";
   //isActive
   bool isActive = true;
 
@@ -69,22 +71,23 @@ class _ClientsEditDialogState extends State<ClientsEditDialog> {
   void init() {
     final c = client!;
     name = c.name;
-    companyController.text = c.company ?? "";
-    emailController.text = c.email ?? "";
-    phoneController.text = c.phone ?? "";
+    company = c.company ?? "";
+    email = c.email ?? "";
+    phone = c.phone ?? "";
+    fax = c.fax ?? "";
 
-    address1Controller.text = c.address.line1;
-    address2Controller.text = c.address.line2;
-    cityController.text = c.address.city;
-    countyController.text = c.address.county;
+    address1 = c.address.line1;
+    address2 = c.address.line2;
+    city = c.address.city;
+    county = c.address.county;
     country = c.address.getCountryMd(lists.countries);
-    postcodeController.text = c.address.postcode;
+    postcode = c.address.postcode;
 
     currency = c.getCurrencyMd(lists.currencies);
     paymentMethod = c.getPaymentMethodMd(lists.paymentMethods);
-    paymentDueController.text = c.payingDays.toString();
+    paymentDue = c.payingDays.toString();
     invoicePeriod = c.getInvoicePeriod(lists.invoicePeriods);
-    invoiceDayController.text = c.invoiceDay?.toString() ?? "";
+    invoiceDay = c.invoiceDay?.toString() ?? "";
     isActive = c.active;
 
     setState(() {});
@@ -92,25 +95,240 @@ class _ClientsEditDialogState extends State<ClientsEditDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultDialog(
-        builder: (context) {
-          return Column(
-            children: [
-              ShiftCard(
-                  title: "Personal Info",
+    return AlertDialog(
+        scrollable: true,
+        content: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                ShiftCard(
+                    title: "Personal Info",
+                    width: context.width * 0.5,
+                    items: [
+                      ShiftCardItem(
+                          title: "Name",
+                          simpleText: name,
+                          onChanged: (value) {
+                            name = value;
+                          }),
+                      ShiftCardItem(
+                          title: "Company",
+                          simpleText: company,
+                          onChanged: (value) {
+                            company = value;
+                          }),
+                      ShiftCardItem(
+                          title: "Email",
+                          simpleText: email,
+                          onChanged: (value) {
+                            email = value;
+                          }),
+                      ShiftCardItem(
+                          title: "Phone",
+                          simpleText: phone,
+                          onChanged: (value) {
+                            phone = value;
+                          }),
+                      ShiftCardItem(
+                          title: "Fax",
+                          simpleText: fax,
+                          onChanged: (value) {
+                            fax = value;
+                          }),
+                    ]),
+                const SizedBox(height: 20),
+                ShiftCard(
+                  title: "Payment Information",
                   width: context.width * 0.5,
                   items: [
                     ShiftCardItem(
-                        title: "Name",
-                        simpleText: name,
-                        maxLines: 2,
+                      title: "Currency",
+                      dropdown: ShiftCardDropdown(
+                        additionalValueId: currency?.code,
+                        label: currency?.sign,
                         onChanged: (value) {
-                          name = value;
-                        }),
-                  ]),
-            ],
-          );
-        },
-        title: "${isNew ? "Add" : "Edit"} Client");
+                          final index = lists.currencies
+                              .indexWhere((e) => e.code == value.additionalId);
+                          if (index != -1) {
+                            currency = lists.currencies[index];
+                            setState(() {});
+                          }
+                        },
+                        items: [
+                          for (int i = 0; i < lists.currencies.length; i++)
+                            DefaultMenuItem(
+                                id: i,
+                                title:
+                                    "${lists.currencies[i].sign} - ${lists.currencies[i].code}",
+                                additionalId: lists.currencies[i].code),
+                        ],
+                      ),
+                    ),
+                    ShiftCardItem(
+                        title: "Payment method",
+                        dropdown: ShiftCardDropdown(
+                          valueId: paymentMethod?.id,
+                          label: paymentMethod?.name,
+                          items: lists.paymentMethods.map(
+                              (e) => DefaultMenuItem(id: e.id, title: e.name)),
+                          onChanged: (value) {
+                            final method = lists.paymentMethods
+                                .firstWhereOrNull(
+                                    (element) => element.id == value.id);
+                            paymentMethod = method;
+                            setState(() {});
+                          },
+                        )),
+                    ShiftCardItem(
+                        title: "Invoice Period",
+                        dropdown: ShiftCardDropdown(
+                          valueId: invoicePeriod?.id,
+                          label: invoicePeriod?.name,
+                          items: lists.invoicePeriods.map(
+                              (e) => DefaultMenuItem(id: e.id, title: e.name)),
+                          onChanged: (value) {
+                            final method = lists.invoicePeriods
+                                .firstWhereOrNull(
+                                    (element) => element.id == value.id);
+                            invoicePeriod = method;
+                            setState(() {});
+                          },
+                        )),
+                    ShiftCardItem(
+                      title: "Invoice Day",
+                      simpleText: invoiceDay,
+                      onChanged: (value) {
+                        invoiceDay = value;
+                      },
+                    ),
+                    ShiftCardItem(
+                      title: "Comment",
+                      simpleText: comment,
+                      maxLines: 2,
+                      onChanged: (value) {
+                        comment = value;
+                      },
+                    ),
+                    ShiftCardItem(
+                      title: "Active",
+                      checked: isActive,
+                      onChecked: (value) {
+                        isActive = value;
+                        setState(() {});
+                      },
+                    ),
+                    ShiftCardItem(
+                      title: "Combine Invoices",
+                      checked: isCombineInvoices,
+                      onChecked: (value) {
+                        isCombineInvoices = value;
+                        setState(() {});
+                      },
+                    ),
+                    ShiftCardItem(
+                      title: "Send Invoices",
+                      checked: isSendInvoices,
+                      onChecked: (value) {
+                        isSendInvoices = value;
+                        setState(() {});
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(width: 20),
+            ShiftCard(
+              title: "Address",
+              width: context.width * 0.4,
+              items: [
+                ShiftCardItem(
+                  title: "Search Address",
+                  customWidget: AddressAutocompleteWidget(
+                    width: 350,
+                    onSelected: (value) {
+                      print(value);
+                      address1 = value.line1;
+                      address2 = value.line2;
+                      city = value.city;
+                      county = value.county;
+                      country = value.country;
+                      postcode = value.postcode;
+                      setState(() {});
+                    },
+                  ),
+                ),
+                ShiftCardItem(
+                    title: "Address Line 1",
+                    simpleText: address1,
+                    onChanged: (value) {
+                      address1 = value;
+                    }),
+                ShiftCardItem(
+                    title: "Address Line 2",
+                    simpleText: address2,
+                    onChanged: (value) {
+                      address2 = value;
+                    }),
+                ShiftCardItem(
+                    title: "City",
+                    simpleText: city,
+                    onChanged: (value) {
+                      city = value;
+                    }),
+                ShiftCardItem(
+                    title: "County",
+                    simpleText: county,
+                    onChanged: (value) {
+                      county = value;
+                    }),
+                ShiftCardItem(
+                    title: "Country",
+                    dropdown: ShiftCardDropdown(
+                      additionalValueId: country?.code,
+                      label: country?.name,
+                      onChanged: (value) {
+                        final index = lists.countries
+                            .indexWhere((e) => e.code == value.additionalId);
+                        if (index != -1) {
+                          country = lists.countries[index];
+                          setState(() {});
+                        }
+                      },
+                      items: [
+                        for (int i = 0; i < lists.countries.length; i++)
+                          DefaultMenuItem(
+                              id: i,
+                              title: lists.countries[i].name,
+                              additionalId: lists.countries[i].code),
+                      ],
+                    )),
+                ShiftCardItem(
+                    title: "Postcode",
+                    simpleText: postcode,
+                    onChanged: (value) {
+                      postcode = value;
+                    }),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          //Cancel
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: context.theme.colorScheme.secondary),
+              onPressed: () {
+                context.pop();
+              },
+              child: Text("Cancel")),
+          //Submit
+          ElevatedButton(onPressed: onSubmit, child: Text("Submit")),
+        ],
+        title: Text("${isNew ? "Add" : "Edit"} Client"));
   }
+
+  void onSubmit() {}
 }
