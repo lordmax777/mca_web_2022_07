ffbf5562867dd12a04c7f7d76b75f90a71bc99d7
Shokhjakhon <komiljonovshohjahon@gmail.com>
2023-11-07 17:23:03 +0500

    update website

--- website/flutter_service_worker.js
+++ website/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "31a3aa7f19c181c1f19c1330c44a13f0",
-"index.html": "468b3685af9e6e158c6c73ff61f67b9e",
-"/": "468b3685af9e6e158c6c73ff61f67b9e",
+"index.html": "b374ef7e2dae603e6f18bf499f26ba15",
+"/": "b374ef7e2dae603e6f18bf499f26ba15",
 "styles.css": "67e810bc9214d7c3eaba5f0efed1a74e",
-"main.dart.js": "4fbbcdb38dc101427cc7535d400f1e75",
+"main.dart.js": "be79e3feb8647d647d634f5348e07e67",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
